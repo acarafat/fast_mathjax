9ef576a8a6d57f3947040fb51a72c69de444116a
Mubtasim Fuad <m.fuad.rafi@gmail.com>
2021-08-12 12:41:05 +0600

    Update super_mathjax.php

--- super_mathjax.php
+++ super_mathjax.php
@@ -14,20 +14,21 @@ add_action(‘init’, ‘addMathJaxConfig’);
 
 function addMathJaxConfig() {
   return "
-  <script src=\"https://polyfill.io/v3/polyfill.min.js?features=es6\"></script>\n 
-  <script type=\"text/javascript\" id=\"MathJax-script\" async src=\"https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js\"></script>\n 
-  <script>\n window.MathJax = {\n 
-	  tex: {\n 
-		  inlineMath: [ ['$','$'],['\\\(','\\\)'] ],\n 
-		  displayMath: [ ['$$','$$'], ['\\\[','\\\]'] ],\n 
-		  processEscapes: true,\n 
-		  processRefs: true,\n 
-		  packages: ['base', 'ams'] \n 
-		  },	\n 
-		  options: { \n
-			  ignoreHtmlClass: 'tex2jax_ignore|editor-rich-text' \n 
-		  } \n 
-	  }; \n 
+  <script src=\"https://polyfill.io/v3/polyfill.min.js?features=es6\"></script> \n 
+  <script type=\"text/javascript\" id=\"MathJax-script\" async src=\"https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js\"></script> \n 
+  <script> \n 
+	  window.MathJax = { \n 
+		  tex: { \n 
+			  inlineMath: [ ['$','$'],['\\\(','\\\)'] ],\n 
+			  displayMath: [ ['$$','$$'], ['\\\[','\\\]'] ],\n 
+			  processEscapes: true,\n 
+			  processRefs: true,\n 
+			  packages: ['base', 'ams'] \n 
+			}, \n 
+	          options: { \n
+		  	  	ignoreHtmlClass: 'tex2jax_ignore|editor-rich-text' \n 
+			  } \n 
+		  }; \n 
   </script>";
 }
 
