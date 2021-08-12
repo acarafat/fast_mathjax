6a751902f10ba921e62efe560e49ddc77021ab62
Arafat Rahman <ac.arafat@gmail.com>
2021-08-11 21:40:10 -0700

    Create super_mathjax.php

---
+++ super_mathjax.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Plugin Name: Super MathJax
+Plugin URI: https://github.com/acarafat/fast_mathjax/
+Description: A plugin where you can use simple shortcode to load MathJax-LaTeX both inline and display.
+
+Version: 1.0
+
+Author: @acarafat
+Author URI: https://bigganblog.org
+
+License: GPLv3
+
+*/
+
+?>
